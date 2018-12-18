2a187bc377ec6b3df9419a9f50720845a9eea2db
LaikaFusion <newrad4@gmail.com>
2018-12-18 13:01:08 -0500

    module one done

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,19 @@
+import Nav from './Nav'
+
+const Header = () => ( <div>
+  <div className="bar">
+    <a href="" className="href">
+      Sick Fits
+    </a>
+    <Nav/>
+  </div>
+  <div className="sub-bar">
+    <p>Search</p>
+  </div>
+  <div>Cart</div>
+</div>)
+ 
+
+
+
+export default Header;
